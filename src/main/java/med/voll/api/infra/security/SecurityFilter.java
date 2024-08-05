7ce8d9f58285a7a0a3a6757d5d3b60c954860e1e
Heithor Urquiza <111160912+HeithorUrquiza@users.noreply.github.com>
2024-08-05 15:37:13 -0300

    add comments

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -28,13 +28,16 @@ public class SecurityFilter extends OncePerRequestFilter {
         String tokenJWT = this.recoverToken(request);
         if (tokenJWT != null) {
             String subject = this.tokenService.getSubject(tokenJWT);
+//            Validate user
             UserDetails user = this.userRepository.findByLogin(subject);
+//            Do an authentication by Spring
             Authentication authentication = new UsernamePasswordAuthenticationToken(user,null, user.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
         filterChain.doFilter(request, response);
     }
 
+//    Recover token from authorization header
     private String recoverToken (HttpServletRequest request) {
         var authorizationHeader = request.getHeader ( "Authorization" );
         if (authorizationHeader != null) {
